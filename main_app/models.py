27603f54a5aff4ff233e4da916d4224a239fdb27
username <youremail@domain.com>
2022-08-29 16:02:11 -0700

    create, update, delete

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Dog(models.Model):
@@ -6,3 +7,6 @@ class Dog(models.Model):
     breed = models.CharField(max_length=100)
     description = models.TextField(max_length=250)
     age = models.IntegerField()
+
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'dog_id': self.id})
