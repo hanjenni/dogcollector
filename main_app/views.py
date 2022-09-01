9f0a4b9668555d588fa41ff93aac9486ca3264b8
username <youremail@domain.com>
2022-08-31 21:01:17 -0700

    clean up code

--- main_app/views.py
+++ main_app/views.py
@@ -28,7 +28,7 @@ def dogs_detail(request, dog_id):
         'feeding_form': feeding_form,
         'toys': toys_dog_doesnt_have
         })
-        
+
 def assoc_toy(request, dog_id, toy_id):
     Dog.objects.get(id=dog_id).toys.add(toy_id)
     return redirect('detail', dog_id)
@@ -53,8 +53,6 @@ class DogUpdate(UpdateView):
 class DogDelete(DeleteView):
     model = Dog
     success_url= '/dogs/'
-
-
 
 class ToyList(ListView):
     model = Toy
